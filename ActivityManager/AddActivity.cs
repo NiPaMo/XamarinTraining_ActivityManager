73e4b5bdbf278b0e40c9a8577d16af1f8bac2af4
Nick Moyer <nick.moyer@awh.net>
2019-01-14 22:47:04 -0500

    Set up material design and the floating action bar

---
+++ ActivityManager/AddActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content.Res;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Content;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace ActivityManager
+{
+    [Activity(Label = "Material Design", Theme = "@style/MyTheme", MainLauncher = true)]
+    public class AddActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.main);
+        }
+	}
+}
+
--- ActivityManager/MainActivity.cs
+++ ActivityManager/MainActivity.cs
@@ -1,53 +1,92 @@
 ﻿using System;
 using Android.App;
+using Android.Content;
+using Android.Content.Res;
 using Android.OS;
-using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Support.V4.Content;
 using Android.Support.V7.App;
 using Android.Views;
+using Android.Views.Animations;
 using Android.Widget;
 
-namespace ActivityManager
+namespace ActivityManager.Activities
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    [Activity(Label = "Material Design", Theme = "@style/MyTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
+            SetContentView(Resource.Layout.main);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-        }
+            var activityList = FindViewById<ListView>(Resource.Id.activityList);
 
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
+            var activityService = new ActivityService();
+            var activities = activityService.FetchActivityList();
+            activityList.Adapter = new ActivityAdapter(activities, this);
 
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            activityList.Touch += ListItemTouch;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
             {
-                return true;
+                Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                Window.SetStatusBarColor(new Android.Graphics.Color(ContextCompat.GetColor(Application, Resource.Color.primary_dark)));
             }
 
-            return base.OnOptionsItemSelected(item);
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab_add);
+            fab.Click += Fab_Click;
+
+            fab.Visibility = ViewStates.Invisible;
+
         }
 
-        private void FabOnClick(object sender, EventArgs eventArgs)
+        void ListItemTouch(object sender, View.TouchEventArgs e)
         {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            var currentNightMode = (int)Resources.Configuration.UiMode & (int)UiMode.NightYes;
+
+            if (currentNightMode == (int)UiMode.NightNo)
+            {
+                Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightYes);
+            }
+            else
+            {
+                Delegate.SetLocalNightMode(AppCompatDelegate.ModeNightNo);
+            }
+            Recreate();
         }
-	}
+
+        void Fab_Click(object senser, EventArgs e)
+        {
+            var intent = new Intent(this, typeof(AddActivity));
+
+            StartActivity(intent, null);
+        }
+
+        private void AnimateShow(View view)
+        {
+            var anim = new ScaleAnimation(0, 1, 0, 1, .5f, .5f);
+            anim.FillBefore = true;
+            anim.FillAfter = true;
+            anim.FillEnabled = true;
+            anim.Duration = 300;
+            anim.StartOffset = 500;
+
+            anim.Interpolator = new OvershootInterpolator();
+            view.StartAnimation(anim);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnRestart();
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab_add);
+            if (fab.Visibility == ViewStates.Invisible)
+            {
+                AnimateShow(fab);
+            }
+        }
+    }
 }
 
